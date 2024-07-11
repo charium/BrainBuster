19bbd7638f76b589f5c5b45bc2103794db7cee3c
Chloe Hall <ha11@ad.unc.edu>
2024-07-10 20:24:06 -0400

    Ruleset test

--- db/seed.sql
+++ db/seed.sql
@@ -13,3 +13,15 @@ CREATE TABLE movieA (
     correct BOOLEAN NOT NULL,
     date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE musicQ (
+    id SERIAL PRIMARY KEY,
+    question VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE musicA (
+    id SERIAL NOT NULL,
+    answer TEXT NOT NULL,
+    correct BOOLEAN NOT NULL,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
