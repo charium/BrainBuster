fac72774ab89c01163593940d3f8e7be9b6bc43a
Chloe Hall <ha11@ad.unc.edu>
2024-07-10 20:18:27 -0400

    Folder and file structure update plus database

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE `BrainBusters_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+\c course_db;
+
+CREATE TABLE movieQ (
+    id SERIAL PRIMARY KEY,
+    question VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE movieA (
+    id SERIAL NOT NULL,
+    answer TEXT NOT NULL,
+    correct BOOLEAN NOT NULL,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
