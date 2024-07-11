72e4dd90dede2799ab10c8fc7ad34b4629295db1
Chloe Hall <ha11@ad.unc.edu>
2024-07-11 18:24:02 -0400

    Add barebones structure to the MVC webpage

---
+++ controllers/home-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Questions, Answers } = require('../models');
+
+// Skeleton from UNC file folders
+router.get('/', async (req, res) => {
+  try {
+    const dbQuestionData = await Questions.findAll({
+      include: [
+        {
+          model: Answers,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const galleries = dbGalleryData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
+    res.render('homepage', {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,15 @@
 console.log('models/index.js')
+
+const User = require('/User');
+const Quiz = require('/Quiz');
+const Question = require('/Question');
+
+Quiz.hasMany(Question, {
+  foreignKey: 'Quiz_id',
+});
+
+Questions.belongsTo(Quiz, {
+  foreignKey: 'Quiz_id',
+});
+
+module.exports = { User, Quiz, Question };
